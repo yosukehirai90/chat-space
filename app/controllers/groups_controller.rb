84b0b7c33917c20bc02099bd61aa5f30e26c4d06
yousukehirai90 <you803842@gmail.com>
2019-02-06 21:35:04 +0900

    group-new commit
    #WHY
    グループ作成機能を追加するため
    #WHAT
    groupコントローラーからgroupビューの作成

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,11 +3,23 @@ class GroupsController < ApplicationController
   end
 
   def new
+    @group = Group.new
+    @group.users << current_user
   end
 
   def create
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
   end
 
   def update
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, { :uer_ids => [] })
   end
 end
