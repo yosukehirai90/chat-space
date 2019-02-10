8ee76af3e36844e02a4f4a5f1bf5d7887c6ac371
yousukehirai90 <you803842@gmail.com>
2019-02-10 14:19:42 +0900

    message-submit commit
    #WHY
    送信機能をつけるため
    #WHAT
    message.html.hamlの編集
    controllerの編集

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,7 +6,7 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @message = group.messages.new(message_params)
+    @message = @group.messages.new(message_params)
     if @message.save
       redirect_to group_messages_path(@group), notice: 'メッセージが送信されました。'
     else
